0dec66b925de06f8f3f0ff1f61a50c7888fc7733
Scott Koland <skoland@compositgroup.com>
2016-09-20 09:27:01 -0700

    Add project files.

---
+++ AspNetInfiniteScroll2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetInfiniteScroll2.MvcApplication" Language="C#" %>
---
+++ AspNetInfiniteScroll2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetInfiniteScroll2.Startup))]
+namespace AspNetInfiniteScroll2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
